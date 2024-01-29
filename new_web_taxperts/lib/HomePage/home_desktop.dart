92d9cb010f3e12ed8fda33466553e00ba6595611
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2024-01-29 15:38:24 +0530

    Top Black Bar adde

--- new_web_taxperts/lib/HomePage/home_desktop.dart
+++ new_web_taxperts/lib/HomePage/home_desktop.dart
@@ -11,7 +11,121 @@ class _HomeDesktopState extends State<HomeDesktop> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Desktop')),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 60,
+            decoration: BoxDecoration(color: Colors.black),
+            child: Row(
+              children: [
+                Expanded(
+                    flex: 4,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: Icon(Icons.email_rounded,color: Colors.white,),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text('connect@taxperts.lk',style: TextStyle(fontSize: 15,color: Colors.white),),
+                        )
+                      ],
+                    )),
+                Expanded(
+                    flex: 3,
+                    child: Row(
+                      children: [
+                        Icon(Icons.phone_in_talk_rounded,color: Colors.white,),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text('+94 777 148 839',style: TextStyle(fontSize: 15,color: Colors.white),),
+                        )
+                      ],
+                    )),
+                Expanded(
+                    flex: 6,
+                    child: Row(
+                      children: [
+                        Icon(Icons.location_on,color: Colors.white,),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text('No. 101, Olcott Mawatha, Colombo 11',style: TextStyle(fontSize: 15,color: Colors.white),),
+                        )
+                      ],
+                    )),
+                Expanded(
+                    flex: 5,
+                    child: Row(
+                      children: [
+                        Icon(Icons.watch_later_rounded,color: Colors.white,),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text('Monday - Friday: 8.30 - 17.30',style: TextStyle(fontSize: 15,color: Colors.white),),
+                        )
+                      ],
+                    )),
+                Expanded(
+                  flex: 3,
+                  child: Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        width: 35,
+                        height: 35,
+                        padding: EdgeInsets.all(9),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                        ),
+                        child: Image.asset('images/fbIcon.png'),
+                      ),
+                      Container(
+                        width: 35,
+                        height: 35,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                        ),
+                        child: Image.asset('images/twIcon.png'),
+                      ),
+                      Container(
+                        width: 35,
+                        height: 35,
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                        ),
+                        child: Image.asset('images/inIcon.png'),
+                      ),
+                      Container(
+                        width: 35,
+                        height: 35,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                        ),
+                        child: Image.asset('images/ytIcon.png'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Center(child: Text('Desktop')),
+
+          Container(
+            width: double.infinity,
+            height: 100,
+            color: Colors.black87,
+          )
+        ],
+      ),
     );
   }
 }
--- new_web_taxperts/lib/responsive.dart
+++ new_web_taxperts/lib/responsive.dart
@@ -16,17 +16,17 @@ class Responsive extends StatelessWidget {
       MediaQuery.of(context).size.width < 600;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1000 &&
+      MediaQuery.of(context).size.width < 1070 &&
           MediaQuery.of(context).size.width >= 600;
 
   static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 1000;
+      MediaQuery.of(context).size.width >= 1070;
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (constraints.maxWidth >= 1000) {
+        if (constraints.maxWidth >= 1070) {
           return desktop;
         } else if (constraints.maxWidth >= 600) {
           return tablet;
