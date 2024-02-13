38876ca2cfaa9cf7525617be3578d5601cd3ffda
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2024-02-14 00:16:53 +0530

    Form Page connected to database

--- new_web_taxperts/lib/CalculatorComponants.dart
+++ new_web_taxperts/lib/CalculatorComponants.dart
@@ -145,4 +145,12 @@ class RedioContainer extends StatelessWidget {
   }
 }
 
+Future<void> snackBar(BuildContext context, String message, Color color) async {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(
+      message,
+      style: TextStyle(color: color, fontSize: 17.0),
+    ),
+  ));
+}
 
--- new_web_taxperts/lib/FormPage/form_desktop.dart
+++ new_web_taxperts/lib/FormPage/form_desktop.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:new_web_taxperts/Componants.dart';
-
+import 'package:http/http.dart' as http;
+import '../CalculatorComponants.dart';
 import '../colors.dart';
 
 class FormDesktop extends StatefulWidget {
@@ -11,6 +14,77 @@ class FormDesktop extends StatefulWidget {
 }
 
 class _FormDesktopState extends State<FormDesktop> {
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
+  String? _selectedTaxType;
+  String? _hasTIN;
+
+  Future<void> addSubmission(BuildContext context) async {
+    // Validate input fields
+    if (_nameController.text.trim().isEmpty ||
+        _phoneController.text.trim().isEmpty ||
+        _emailController.text.trim().isEmpty ||
+        _messageController.text.isEmpty ) {
+      // Show an error message if any of the required fields are empty
+      snackBar(context, "Please fill in all required fields", Colors.red);
+      return;
+    }
+
+    // Other validation logic can be added here
+
+    // If all validations pass, proceed with the registration
+    var url = "http://dev.workspace.cbs.lk/addSubmission.php";
+
+    var data = {
+      "name_": _nameController.text.trim(),
+      "tax_type": _selectedTaxType,
+      "email": _emailController.text.toString().trim(),
+      "phone": _phoneController.text.toString().trim(),
+      "tin": _hasTIN,
+      "message_": _messageController.text.toString().trim(),
+      "from_": 'Callback Form',
+      "device": 'Desktop',
+      "active": '1',
+      "read_status": '0',
+      "actions": 'None',
+    };
+
+    http.Response res = await http.post(
+      Uri.parse(url),
+      body: data,
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      encoding: Encoding.getByName("utf-8"),
+    );
+
+    if (res.statusCode.toString() == "200") {
+      if (jsonDecode(res.body) == "true") {
+        if (!mounted) return;
+        showSuccessSnackBar(context); // Show the success SnackBar
+      } else {
+        if (!mounted) return;
+        snackBar(context, "Error", Colors.yellow);
+      }
+    } else {
+      if (!mounted) return;
+      snackBar(context, "Error", Colors.redAccent);
+    }
+  }
+
+  void showSuccessSnackBar(BuildContext context) {
+    final snackBar = SnackBar(
+      content:
+      Text('Submission successful! We will contact you soon!'),
+      backgroundColor: Colors.green, // You can customize the color
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,215 +107,205 @@ class _FormDesktopState extends State<FormDesktop> {
             ),
           ),
 
-        Container(
-          color: Colors.white,
-          margin: EdgeInsets.symmetric(horizontal: 160,vertical: 50),
+          Container(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(horizontal: 160,vertical: 50),
 
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                height: 70,
-                width: double.infinity,
-                color: AppColor.darkTextGreen,
-                child: Center(
-                  child: Text(
-                    'Request a callback',
-                    style: TextStyle(
-                        fontFamily: 'Inter',
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: 80,
+                  width: double.infinity,
+                  color: AppColor.darkTextGreen,
+                  child: Center(
+                    child: Text(
+                      'Request a callback',
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              // Container(
-              //   height: 100,
-              //   color: Colors.grey.shade700,
-              // ),
-              // SizedBox(height: 20.0),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: CustomFormField2(
-                      label: 'Type of taxes:  ',
-                      child: DropdownButtonFormField(
-                        items: ['Individual Income Tax (IIT)',
-                          'Corporate Income Tax (CIT)',
-                          'Partnership Income Tax (PIT) ',
-                          'Value Added Tax (VAT)',
-                          'Advance Personal Income Tax (APIT)',
-                          'Advance Income Tax (AIT)',
-                          'Capital Gain Tax (CGT)',
-                          'Simplified Value Added Tax (SVAT)',
-                          'Stamp Duty (SD)',
-                          'Other Taxes ',
-                          'Transfer Pricing',
-                          'International Double Taxation',
-                          'Expat Taxation',
-                          'Tax Advisory Services',
-                          'Return Compliance',
-                          'Social Security Contribution Levy (SSCL)',
-                          'With Holding Tax (WHT)'].map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (value) {},
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
+                SizedBox(height: 20.0),
+                // Container(
+                //   height: 100,
+                //   color: Colors.grey.shade700,
+                // ),
+                // SizedBox(height: 20.0),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: CustomFormField(
+                        label: 'Type of taxes:  ',
+                        child: DropdownButtonFormField(
+                          value: _selectedTaxType,
+                          items: ['Individual Income Tax (IIT)', 'Other'].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedTaxType = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(width: 10.0),
-                  Expanded(
-                    child: CustomFormField2(
-                      label: 'Name: ',
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
+                    SizedBox(width: 20.0),
+                    Expanded(
+                      child: CustomFormField(
+                        label: 'Name: ',
+                        child: TextField(
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: CustomFormField2(
-                      label: 'Email: ',
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: CustomFormField(
+                        label: 'Email: ',
+                        child: TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(width: 20.0),
-                  Expanded(
-                    child: CustomFormField2(
-                      label: 'Phone Number: ',
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
+                    SizedBox(width: 20.0),
+                    Expanded(
+                      child: CustomFormField(
+                        label: 'Phone Number: ',
+                        child: TextField(
+                          controller: _phoneController,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:40.0),
+                  child: Text(
                     'Do you have Taxpayer Identification Number (TIN)?',
                     style: TextStyle(
                       fontSize: 20.0,
-                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:40.0),
+                  child: Row(
+                    children: <Widget>[
                       Radio(
                         value: 'Yes',
-                        groupValue: null, // Replace _tinOption with your state variable
+                        groupValue: _hasTIN,
                         onChanged: (value) {
-
+                          setState(() {
+                            _hasTIN = value;
+                          });
                         },
                       ),
-                      Text(
-                        'Yes',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                        ),
-                      ),
+                      Text('Yes'),
                       Radio(
                         value: 'No',
-                        groupValue: null, // Replace _tinOption with your state variable
+                        groupValue: _hasTIN,
                         onChanged: (value) {
-
+                          setState(() {
+                            _hasTIN = value;
+                          });
                         },
                       ),
-                      Text(
-                        'No',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                        ),
-                      ),
+                      Text('No'),
                     ],
                   ),
-                ],
-              ),
                 ),
-
-            SizedBox(height: 20.0),
-              CustomFormField2(
-                label: 'Message: ',
-                child: TextField(
-                  decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
+                SizedBox(height: 20.0),
+                CustomFormField(
+                  label: 'Message: ',
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    keyboardType: TextInputType.multiline,
+                    minLines: 3,
+                    maxLines: 5,
                   ),
                 ),
-                  keyboardType: TextInputType.multiline,
-                  minLines: 3,
-                  maxLines: 5,
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // TODO: Submit callback request
-                  },
-                  child: Text('Submit',style: TextStyle(
-                    fontSize: 18
-                  ),),
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(
-                        120, 40), // Set the width and height
-                    primary: AppColor
-                        .buttonGreen, // Set the background color to green
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                          5), // Set the border radius
+                SizedBox(height: 20.0),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // TODO: Submit callback request
+                      print('Type of taxes: $_selectedTaxType');
+                      print('Name: ${_nameController.text}');
+                      print('Email: ${_emailController.text}');
+                      print('Phone Number: ${_phoneController.text}');
+                      print('TIN: $_hasTIN');
+                      print('Message: ${_messageController.text}');
+
+                      addSubmission(context);
+
+                    },
+                    child: Text('Submit',style: TextStyle(
+                        fontSize: 18
+                    ),),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(
+                          120, 40), // Set the width and height
+                      primary: AppColor
+                          .buttonGreen, // Set the background color to green
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            5), // Set the border radius
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              SizedBox(height: 20.0),
-            ],
+                SizedBox(height: 20.0),
+              ],
+            ),
           ),
-        ),
           WFooter(),
         ],
         ),
